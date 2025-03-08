0bdab7fe28f25db3a3cad9324f46b67e296eae35
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2025-03-08 22:46:32 +0200

    finish

--- task.sql
+++ task.sql
@@ -4,8 +4,12 @@ USE ShopDB;
 -- Some data should be created outside the transaction (here)
 
 -- Start the transaction 
-START TRANSACTION; 
+START TRANSACTION;
+INSERT INTO Orders (СustomerID, Date)
+VALUES('1', '2023-01-01');
+INSERT INTO OrderItems (ProductID, Count, OrderId)
+VALUES('1', 1, '1');
+UPDATE Orders SET WarehouseAmount = WarehouseAmount - 5 WHERE OrderID = '1';
 
--- And some data should be created inside the transaction 
 
-COMMIT; 
+COMMIT;
